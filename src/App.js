66efbfa327af32cc14175a29e7107359be6e4b68
Juan Carlos Concha <jnkarlos.c@gmail.com>
2020-09-23 21:35:13 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
+import Axios from 'axios';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Card from './Card'
+import Spinner from "./Spinner";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      searchName: '',
+      heroes: []
+    }
+
+    this.handleSearchNameChange = this.handleSearchNameChange.bind(this)
+  }
+  handleSearchNameChange(event) {
+    this.setState({ searchName: event.target.value }, () =>
+      this.searchSuperHeroByName(this.state.searchName)
+    );
+  }
+
+  searchSuperHeroByName(name) {
+    this.setState({ loading: true, heroes: [] });
+    Axios.get("https://superheroapi.com/api.php/692608721639099/search/" + name)
+      .then(response => response.data)
+      .then(data => {
+        if (data.response === "success") {
+          this.setState({ heroes: data.results });
+        } else {
+          this.setState({ heroes: [] });
+        }
+        this.setState({ loading: false });
+      });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Super Heroes App</h1>
+        <div className="container">
+          <div className="input-group mb-3">
+            <input type="text"
+              placeholder="Ingresar nombre super heroe"
+              className="form-control"
+              value={this.state.searchName}
+              onChange={this.handleSearchNameChange}
+            />
+          </div>
+        </div>
+        <div className="container d-flex flex-wrap justify-content-center">
+          {this.state.loading && <Spinner />}
+          {this.state.heroes.map(hero => {
+            return (
+              <Card key={hero.id}
+                name={hero.name}
+                imageUrl={hero.image.url}
+              />
+            )
+          })}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Card.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Card = (props) => {
+  return (
+    <div className="card w-25 mr-2 mb-2">
+      <img className="card-img-top" src={props.imageUrl} alt="superhero" />
+      <div className="card-body">
+        <p className="card-text">{props.name}</p>
+      </div>
+    </div>
+  )
+};
+
+export default Card;
